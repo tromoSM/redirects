dfe8e8ee8e8e2c9d8e245feee8aaa839f3d1c67e
tromoSM <tromosm7@gmail.com>
2026-06-25 04:25:04 +0530

    added titles

--- redirect.js
+++ redirect.js
@@ -1,4 +1,12 @@
 window.addEventListener('DOMContentLoaded',async function(){
+    function sleep(dih){
+        return new Promise(resolve=>setTimeout(resolve,dih))
+    }
+    function refreshtitle(title){
+        let t=document.createElement('title')
+        document.querySelector('head').append(t)
+        t.innerHTML=title
+    }
     let params=new URLSearchParams(window.location.search)
     let path=''
     let infojson=''
@@ -19,17 +27,21 @@ window.addEventListener('DOMContentLoaded',async function(){
     }
     else{
         alert('No Path to redirect to. use ?key=path')
+        
     }
     if(path.startsWith("git_")){
+        refreshtitle('redirecting to github')
         goto(`https://github.com/tromoSM/${path.split('git_',2)[1]}`)
     }
     else if(path.startsWith('page_')){
+        refreshtitle(`redirecting to ${infojson.redirections.root}`)
         goto(`${infojson.redirections.root}${path.split('page_',2)[1].replaceAll(inlineseperator,'/')}`)
     }
-
+    
     else if(path!=''){
      await fetch("https://raw.githubusercontent.com/tromoSM/tromoSM-assets/main/redirects/redirects.json").then(main=>main.json()).then(main=>{
         if(path.startsWith('download_')){
+            refreshtitle('starting download')
             try{
                 if(params.get('type')){
                  goto(`${main.redirects.download[path.split('download_',2)[1]][params.get('type')][params.get('os')]}`)
