772d5c755b7955f43a47b8318368d2489550bb71
tromoSM <tromosm7@gmail.com>
2026-06-26 00:34:49 +0530

    Revert "move version to each download"
    This reverts commit e3c2e13c8843f1ec8c44f57516ce17588a8ed012.

--- redirect.js
+++ redirect.js
@@ -86,11 +86,6 @@ window.addEventListener('DOMContentLoaded',async function(){
                     desc.setAttribute('fallb','')
                     document.body.append(desc)
                     desc.setAttribute('hidden','')
-                    // 
-                    let infop=document.createElement('p')
-                    infop.innerHTML`
-                    <span></span>
-                    `
                 }
             }
             refreshtitle('starting download')
